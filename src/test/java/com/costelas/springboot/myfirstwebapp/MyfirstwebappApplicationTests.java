92ebd3a0aa2178b23dd2ace96631f165c448a057
denos <costelasdenis@gmail.com>
2023-05-14 16:08:19 +0300

    init commit

---
+++ src/test/java/com/costelas/springboot/myfirstwebapp/MyfirstwebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.costelas.springboot.myfirstwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyfirstwebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
