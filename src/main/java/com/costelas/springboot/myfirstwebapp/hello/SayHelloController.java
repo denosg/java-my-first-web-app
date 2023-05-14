4f01f5d0bd6ed36df8430c74d8bf000ffafa8d42
denos <costelasdenis@gmail.com>
2023-05-14 16:31:06 +0300

    tedious html task

--- src/main/java/com/costelas/springboot/myfirstwebapp/hello/SayHelloController.java
+++ src/main/java/com/costelas/springboot/myfirstwebapp/hello/SayHelloController.java
@@ -8,11 +8,27 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class SayHelloController {
 
     // "say-hello" => "Hello! What are you learning today ?"
-    // when somebody goes to say-hello, we want to receive this string
+    // when somebody goes to say-hello tab, we want to receive this string
     // http://localhost:2002/say-hello
     @RequestMapping("say-hello")
     @ResponseBody // will return whatever is returned by this method as it is to the browser
     public String sayHello() {
         return "Hello! What are you learning today ?";
+    }
+
+    @RequestMapping("say-hello-html")
+    @ResponseBody // will return whatever is returned by this method as it is to the browser
+    public String sayHelloHtml() {
+        StringBuffer stringBuffer = new StringBuffer();
+        stringBuffer.append("<html>");
+        stringBuffer.append("<head>");
+        stringBuffer.append("<title>My first HTML page</title>");
+        stringBuffer.append("</head>");
+        stringBuffer.append("<body>");
+        stringBuffer.append("MY first HTML page with body - Changed");
+        stringBuffer.append("</body>");
+        stringBuffer.append("</html>");
+
+        return stringBuffer.toString();
     }
 }
