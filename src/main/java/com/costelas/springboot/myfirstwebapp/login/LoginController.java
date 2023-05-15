cabd8d3682e661273bf51a9d05ba95c831a831cc
denos <costelasdenis@gmail.com>
2023-05-15 15:52:12 +0300

    Prod Logging

--- src/main/java/com/costelas/springboot/myfirstwebapp/login/LoginController.java
+++ src/main/java/com/costelas/springboot/myfirstwebapp/login/LoginController.java
@@ -1,5 +1,7 @@
 package com.costelas.springboot.myfirstwebapp.login;
 
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,8 +10,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class LoginController {
 
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
     // http://localhost:2002/login?name=costelas
-    // @RequestParam makes it so we get the name parameter for use
+    // @RequestParam makes it, so we get the name parameter for use
 
     // Model passes the name parameter
     // Whenever we want anything available to the .jsp, we add it to the ModelMap
@@ -17,6 +21,9 @@ public class LoginController {
     public String gotoLoginPage(@RequestParam String name, ModelMap modelMap) {
         // adding name to modelMap
         modelMap.put("name", name);
+
+        logger.debug("Request param is {}", name);
+
         System.out.println("Request param is "+ name); //NOT RECOMMENDED FOR PROD CODE
 
         return "login";
