615278523a0bc87e746fe6cac146fb0db42f2267
denos <costelasdenis@gmail.com>
2023-05-15 15:43:03 +0300

    Using ModelMap to show param to page

--- src/main/java/com/costelas/springboot/myfirstwebapp/login/LoginController.java
+++ src/main/java/com/costelas/springboot/myfirstwebapp/login/LoginController.java
@@ -1,13 +1,24 @@
 package com.costelas.springboot.myfirstwebapp.login;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
 
+    // http://localhost:2002/login?name=costelas
+    // @RequestParam makes it so we get the name parameter for use
+
+    // Model passes the name parameter
+    // Whenever we want anything available to the .jsp, we add it to the ModelMap
     @RequestMapping("login")
-    public String gotoLoginPage() {
+    public String gotoLoginPage(@RequestParam String name, ModelMap modelMap) {
+        // adding name to modelMap
+        modelMap.put("name", name);
+        System.out.println("Request param is "+ name); //NOT RECOMMENDED FOR PROD CODE
+
         return "login";
     }
 }
