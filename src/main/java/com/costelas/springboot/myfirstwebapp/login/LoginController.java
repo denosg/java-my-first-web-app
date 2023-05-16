223f6e14611895918066011e73c5ad75091f8860
denos <costelasdenis@gmail.com>
2023-05-16 12:43:54 +0300

    Passing data from login to welcome @RequestParam + ModelMap

--- src/main/java/com/costelas/springboot/myfirstwebapp/login/LoginController.java
+++ src/main/java/com/costelas/springboot/myfirstwebapp/login/LoginController.java
@@ -3,7 +3,10 @@ package com.costelas.springboot.myfirstwebapp.login;
 import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
@@ -15,9 +18,17 @@ public class LoginController {
 
     // Model passes the name parameter
     // Whenever we want anything available to the .jsp, we add it to the ModelMap
-    @RequestMapping("login")
-    public String gotoLoginPage() {
 
+    //GET, POST are being handled by this method
+    @RequestMapping(value = "login",method = RequestMethod.GET)
+    public String gotoLoginPage() {
         return "login";
+    }
+
+    @RequestMapping(value = "login",method = RequestMethod.POST)
+    public String gotoWelcomePage(@RequestParam String name, @RequestParam String password, ModelMap modelMap) {
+        modelMap.put("name", name);
+        modelMap.put("password", password);
+        return "welcome";
     }
 }
---
+++ src/main/resources/META-INF/resources/WEB-INF/jsp/welcome.jsp
@@ -0,0 +1,10 @@
+<html>
+	<head>
+		<title>Welcome Page</title>
+	</head>
+	<body>
+		<div>Welcome to costelas !</div>
+		<div>Your name: ${name}</div>
+		<div>Your password: ${password}</div>
+	</body>
+</html>
