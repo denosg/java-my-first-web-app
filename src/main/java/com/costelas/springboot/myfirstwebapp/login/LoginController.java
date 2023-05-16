798348e47438986b11985dbfde619b9afce34d28
denos <costelasdenis@gmail.com>
2023-05-16 12:33:27 +0300

    Name + Password login

--- src/main/java/com/costelas/springboot/myfirstwebapp/login/LoginController.java
+++ src/main/java/com/costelas/springboot/myfirstwebapp/login/LoginController.java
@@ -3,9 +3,7 @@ package com.costelas.springboot.myfirstwebapp.login;
 import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
@@ -18,13 +16,7 @@ public class LoginController {
     // Model passes the name parameter
     // Whenever we want anything available to the .jsp, we add it to the ModelMap
     @RequestMapping("login")
-    public String gotoLoginPage(@RequestParam String name, ModelMap modelMap) {
-        // adding name to modelMap
-        modelMap.put("name", name);
-
-        logger.debug("Request param is {}", name);
-
-        System.out.println("Request param is "+ name); //NOT RECOMMENDED FOR PROD CODE
+    public String gotoLoginPage() {
 
         return "login";
     }
